67adc970621cd7d4774dffd19d724d478e7e7f75
Kingsleyaham <kigsleyaham6@gmail.com>
2022-07-18 10:38:11 +0100

    made request https

--- assets/js/map.js
+++
@@ -1,9 +0,0 @@
-var map = L.map("map").setView([51.505, -0.09], 15);
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 25,
-  zoomControl: true,
-  attribution: "© OpenStreetMap",
-}).addTo(map);
-
-var marker = L.marker([51.5, -0.09]).addTo(map);
