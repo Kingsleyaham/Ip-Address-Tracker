8e3f059fda25066f8ea1ec6d5f4d93aa24430d8d
Kingsleyaham <kigsleyaham6@gmail.com>
2022-07-18 10:27:30 +0100

    done with project

---
+++ README.md
@@ -0,0 +1,57 @@
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+### Screenshot
+
+![desktop screenshot](./assets/screenshots/desktop.png)
+![mobile screenshot](./assets/screenshots/mobile.png)
+
+### Links
+
+- Solution URL: [github repo](https://github.com/Kingsleyaham/Ip-Address-Tracker)
+- Live Site URL: [ip address tracker](https://kingsleyaham.github.io/Ip-Address-Tracker/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Bootstrap 5
+- Javascript
+
+### Continued development
+
+I want to focus more on the Mern stack and also possibly use Next Js in future projects
+
+## Author
+
+- Linkedin - [@kingsley-aham-282a51225](https://www.linkedin.com/in/kingsley-aham-282a51225/)
+- Frontend Mentor - [@Kingsleyaham](https://www.frontendmentor.io/profile/Kingsleyaham)
+- Twitter - [@aham_kingsley8](https://twitter.com/aham_kingsley8)
---
+++ assets/js/main.js
@@ -0,0 +1,121 @@
+const submitBtn = document.querySelector("#submitBtn");
+
+submitBtn.addEventListener("click", fetchIpInfo);
+
+function fetchIpInfo() {
+  let userInput = document.querySelector("#ipaddress");
+
+  if (isEmpty(userInput)) return;
+
+  let inputVal = userInput.value.trim();
+  let apiKey = "at_gGAEPaFZ0ZiwutDn4tML5oz8Pr2JV";
+  let ipAddress = isIpAddress(inputVal) ? inputVal : "";
+  let domain = isValidUrl(inputVal) ? sanitizeDomain(inputVal) : "";
+
+  userInput.value = "";
+
+  // fetch details using either domain or ipAddress
+  async function fetchDetails(url) {
+    try {
+      const response = await axios.get(url);
+      const data = response.data;
+
+      // return data to DOM
+      document.querySelector("#ipAdd").textContent = data.ip;
+      document.querySelector(
+        "#location"
+      ).textContent = `${data.location.country}, ${data.location.region}`;
+      document.querySelector(
+        "#timezone"
+      ).textContent = `UTC${data.location.timezone}`;
+      document.querySelector("#isp").textContent = data.isp;
+
+      ip = data.ip;
+
+      getLatitudeLongitude(ip);
+
+      // console.log(latLong);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  fetchDetails(
+    `https://geo.ipify.org/api/v2/country?apiKey=${apiKey}&ipAddress=${ipAddress}&domain=${domain}`
+  );
+}
+
+function getLatitudeLongitude(ip) {
+  let url = `http://ip-api.com/json/${ip}`;
+  const latLong = {};
+  (async function (ip) {
+    try {
+      const response = await axios.get(url);
+      const data = response.data;
+
+      latLong.lat = data.lat;
+      latLong.lon = data.lon;
+
+      // display location map
+      displayLocationMap(latLong);
+    } catch (error) {
+      console.error(error);
+    }
+  })();
+}
+
+function displayLocationMap(latLong) {
+  var container = L.DomUtil.get("map");
+
+  if (container != null) {
+    container._leaflet_id = null;
+  }
+
+  var map = L.map("map", {
+    minZoom: 5,
+    maxZoom: 25,
+    zoomControl: false,
+  });
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "© OpenStreetMap",
+  }).addTo(map);
+
+  map.setView([latLong.lat, latLong.lon], 13);
+  var marker = L.marker([latLong.lat, latLong.lon]).addTo(map);
+}
+
+// input validation functions
+
+function isEmpty(str) {
+  return str.length === 0;
+}
+
+function isIpAddress(ip) {
+  let regExp =
+    /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+
+  return regExp.test(ip);
+}
+
+function isValidUrl(domain) {
+  let regExp =
+    /((?:(?:http?|ftp)[s]*:\/\/)?[a-z0-9-%\/\&=?\.]+\.[a-z]{2,4}\/?([^\s<>\#%"\,\{\}\\|\\\^\[\]`]+)?)/gi;
+
+  return regExp.test(domain);
+}
+
+function sanitizeDomain(domain) {
+  var result;
+  var match;
+  if (
+    (match = domain.match(
+      /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n\?\=]+)/im
+    ))
+  ) {
+    result = match[1];
+  }
+  return result;
+}
+
+window.addEventListener("load", fetchIpInfo);
